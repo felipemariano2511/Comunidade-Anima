c83172f2f5686c07af05ecb7ceeab539f07a3f99
Felipe Warchelski <fewarchelski@gmail.com>
2024-06-03 01:06:09 -0300

    Update

---
+++ src/atleticas.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="styles/style-pattern.css">
+    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="styles/atleticas.css">
+    <title>Eventos</title>
+
+
+</head>
+
+<body>
+    <header class="header-main">
+        <div class="container-header">
+            <img src="../imgs/dev/logo-anima-1024-white.png" alt="" width="100px">
+        </div>
+    </header>
+    <nav class="sidebar close">
+        <header>
+            <div class="image-text">
+                <a href="">
+                    <span class="image">
+                        <img src="../imgs/usuario/user-1.webp" alt="">
+                    </span>
+                </a>
+
+
+                <div class="text logo-text">
+                    <span class="name">Felipe</span>
+                    <span class="profession">Ciência da computação</span>
+                </div>
+            </div>
+
+
+        </header>
+
+        <div class="menu-bar">
+            <div class="menu">
+
+                <ul class="menu-links">
+
+                    <li class="nav-link">
+                        <a class="toggle" style="cursor:pointer;">
+                            <i class='bx bx-menu icon' id="menu-icon"></i>
+                            <span class="text nav-text">Menu</span>
+                        </a>
+                    </li>
+
+
+                    <li class="nav-link">
+                        <a href="#">
+                            <i class='bx bx-home-alt icon'></i>
+                            <span class="text nav-text">Home</span>
+                        </a>
+                    </li>
+
+                    <li class="nav-link">
+                        <a href="#">
+                            <i class='bx bx-calendar-event icon'></i>
+                            <span class="text nav-text">Evento</span>
+                        </a>
+                    </li>
+
+                    <li class="nav-link">
+                        <a href="#">
+                            <i class='bx bx-trophy icon'></i>
+                            <span class="text nav-text">Atléticas</span>
+                        </a>
+                    </li>
+
+                    <li class="nav-link">
+                        <a href="#">
+                            <i class='bx bx-shape-triangle icon'></i>
+                            <span class="text nav-text">Comodidades</span>
+                        </a>
+                    </li>
+
+                    <li class="nav-link">
+                        <a href="#">
+                            <i class='bx bx-heart icon'></i>
+                            <span class="text nav-text">Likes</span>
+                        </a>
+                    </li>
+
+                    <li class="nav-link">
+                        <a href="#">
+                            <i class='bx bx-support icon'></i>
+                            <span class="text nav-text">Suporte</span>
+                        </a>
+                    </li>
+
+                </ul>
+            </div>
+
+            <div class="bottom-content">
+                <li class="">
+                    <a href="#">
+                        <i class='bx bx-log-out icon'></i>
+                        <span class="text nav-text">Logout</span>
+                    </a>
+                </li>
+
+            </div>
+        </div>
+
+    </nav>
+
+    <section class="home">
+        <div class="home-title">
+            <h1>Atléticas</h1>
+            <p>Acompanhe nossas atléticas!</p>
+        </div>
+        <div class="atletica-content">
+            <div class="atletica-container">
+                <div class="search-bar">
+                    <i class='bx bx-search'></i>
+                    <input type="search" name="barra-pesquisa" id="" placeholder="Pesquise por atléticas">
+                </div>
+                <div class="atletica-title">
+                    <h1>Mais Relevantes</h1>
+                </div>
+                <div class="atletica-card">
+                    <a href="">Bugatos</a>
+                    <a href="">Intoxicados</a>
+                    <a href="">Avalanche</a>
+                    <a href="">Católitros</a>
+                    <a href="">Jirombinhas</a>
+                    <a href="">#SNPFC</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <script>
+        const body = document.querySelector('body'),
+            sidebar = body.querySelector('nav'),
+            toggle = body.querySelector(".toggle"),
+            searchBtn = body.querySelector(".search-box"),
+            modeSwitch = body.querySelector(".toggle-switch"),
+            modeText = body.querySelector(".mode-text");
+
+
+        toggle.addEventListener("click", () => {
+            sidebar.classList.toggle("close");
+        });
+    </script>
+</body>
+
+</html>
