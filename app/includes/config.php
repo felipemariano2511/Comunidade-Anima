143611a0e8821f60e49db206dba26b7d1ae4c2a8
Felipe Mariano <136535564+felipemariano2511@users.noreply.github.com>
2024-06-04 00:48:43 -0300

    Delete app directory

--- app/includes/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    $con = mysqli_connect("localhost:3500", "root", "");
-    $database = mysqli_select_db($con,"comunidade_anima");
-
-    if (!$con || !$database) echo mysqli_error($con);
-
-?>
