a58fb909e8a7ae83421945330e05905cfdd94c96
Felipe Mariano <136535564+felipemariano2511@users.noreply.github.com>
2024-06-04 00:47:55 -0300

    update

--- public/index.php
+++ public/index.php
@@ -15,7 +15,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Index</title>
     <link rel="stylesheet" href="../src/styles/style.css">
     <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
     <link rel="shortcut icon" href="../imgs/dev/favicon.ico" type="image/x-icon">
