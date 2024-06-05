99d58223000951d2f0648c00f4fb853074f986b6
Felipe Mariano <136535564+felipemariano2511@users.noreply.github.com>
2024-06-04 21:39:34 -0300

    Update atleticas.php

--- src/atleticas.php
+++ src/atleticas.php
@@ -1,3 +1,19 @@
+<?php
+    include '../app/includes/config.php';
+
+    $query = "SELECT * FROM servicos_universitarios WHERE servico = 'Atléticas' situacao = 'ativo'";
+    $result = mysqli_query($con, $query);
+
+    if ($result) {
+        $tableData = array();
+        while ($row = mysqli_fetch_assoc($result)) {
+            $tableData[] = $row;
+        }
+        
+    } else {
+        echo "Sem resultados para essa consulta! ";
+    }
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -5,6 +21,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"><link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
     <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="../src/styles/style-pattern.css">
     <link rel="stylesheet" href="../src/styles/style.css">
@@ -36,6 +53,31 @@
                 </div>
             </div>
         </div>
+        <?php
+        if (is_array($tableData) && !empty($tableData)) {
+            foreach ($tableData as $dados) {
+                if ($dados['situacao'] == "ativo") {
+                    echo '
+                    <div class="cards-main">
+                        <div class="card">
+                            <img src="'.$dados['arquivo'].'" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">'.$dados['titulo'].'</h5>
+                                <p class="card-text">'.$dados['descricao_inicial'].'</p>
+                                <div class="card-buttons">
+                                    <a href="servicos_universitarios.php?id='.$dados['id'].'" class="btn btn-primary">Ver detalhes</a>
+                                    <div class="like-share">
+                                        <i class="bx bx-heart heart-icon"></i>
+                                        <i class="bx bx-share bx-flip-horizontal compartilhar" data-id="'.$dados['id'].'"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>';
+                }
+            }
+        }
+    ?>
     </section>
 
     <script>
