0e80dd9f7e41a9de6678f419a5cc7ceb3014cefe
Felipe Mariano <136535564+felipemariano2511@users.noreply.github.com>
2024-06-07 21:26:05 -0300

    update

---
+++ app/includes/conversor_data.php
@@ -0,0 +1,71 @@
+<?php
+
+    $data_inicial = explode("-", $tableData['data_inicial']);
+    $data_final = explode("-", $tableData['data_final']);
+
+    $ano = $data_inicial[0];
+    $mes = $data_inicial[1];
+    $dia = $data_inicial[2];
+
+    for($i = 0; $i < 2; $i++){
+        switch ($mes){
+            case '01':
+                $mes_formatado = 'Jan';
+                break;
+
+            case '02':
+                $mes_formatado = 'Fev';
+                break;
+
+            case '03':
+                $mes_formatado = 'Mar';
+                break;
+             
+            case '04':
+                $mes_formatado = 'Abr';
+                break;
+
+            case '05':
+                $mes_formatado = 'Mai';
+                break;
+            
+            case '06':
+                $mes_formatado = 'Jun';
+                break;
+
+            case '07':
+                $mes_formatado = 'Jul';
+                break;
+
+            case '08':
+                $mes_formatado = 'Aug';
+                break;
+
+            case '09':
+                $mes_formatado = 'Set';
+                break;
+
+            case '10':
+                $mes_formatado = 'Out';
+                break;
+
+            case '11':
+                $mes_formatado = 'Nov';
+                break;
+
+            case '12':
+                $mes_formatado = 'Dez';
+                break;
+        }
+        if($i == 0){
+            $data_inicial_formatada = $dia.'/'.$mes_formatado.'/'.$ano;
+
+            $ano = $data_final[0];
+            $mes = $data_final[1];
+            $dia = $data_final[2];
+        }else{
+            $data_final_formatada = $dia.'/'.$mes_formatado.'/'.$ano;
+        }
+    }
+
+?>
