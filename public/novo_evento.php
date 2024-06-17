b19d9ca6469cad0e8e6f31cbdc3c3e64dd71ece0
Andre Gonçalves <dregoncalves22@gmail.com>
2024-06-17 10:23:26 -0300

    Refatoração e correções visuais
    Reutilizando os estilos dos formulários e adicionando hover nos botões

--- public/novo_evento.php
+++ public/novo_evento.php
@@ -48,7 +48,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['cadastrar'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../src/styles/style.css">
-    <link rel="stylesheet" href="../src/styles/novo_evento.css">
+    <link rel="stylesheet" href="../src/styles/formularios.css">
     <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
     <link rel="shortcut icon" href="../imgs/dev/favicon.ico" type="image/x-icon">
     <link rel="icon" href="../imgs/dev/favicon.ico" type="image/x-icon">
