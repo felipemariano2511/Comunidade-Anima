536b9d13ddd25ed03df94f2bdee7f9236f05f6cb
Felipe Mariano <136535564+felipemariano2511@users.noreply.github.com>
2024-06-18 14:13:53 -0300

    update

--- public/portal_adm.php
+++ public/portal_adm.php
@@ -23,11 +23,11 @@
 
     switch ($pagina){
         case '':
-            $page = 'portal_adm.php';
+            $page = '../src/portal_adm.php';
             break;
 
         case 'PortalADM':
-            $page = 'portal_adm.php';
+            $page = '../src/portal_adm.php';
             break;
         
         case 'AprovarEventos':
@@ -64,32 +64,6 @@
 <body>
     <?php include "../src/components/main_header.php"; ?>
     <?php include "../src/components/menu.php"; ?>
-    
-
-    <section class="homepage">
-        <div class="home-title">
-            <h1>Portal do Administrador</h1>
-            <p>Aqui você acompanha todas as suas tarefas!</p>
-        </div>
-        <div class="links-container">
-            <div class="links-item">
-                <a href="portal_adm.php?page=AprovarEventos"><img src="../imgs/dev/aprovar_eventos.svg" alt=""></a>
-                <a href="portal_adm.php?page=AprovarEventos" class="button">Aprovar eventos</a>
-            </div link>
-            <div class="links-item">
-                <a href="portal_adm.php?page=GerenciarAtléticas"><img src="../imgs/dev/gerenciamento_atleticas.svg" alt=""></a>
-                <a href="portal_adm.php?page=GerenciarAtléticas" class="button">Gerenciamento de Atléticas</a>
-            </div>
-            <div class="links-item">
-                <a href="portal_adm.php?page=GerenciarComodidades"><img src="../imgs/dev/gerenciamento_comodidades.svg" alt=""></a>
-                <a href="portal_adm.php?page=GerenciarComodidades" class="button">Gerenciamento de Comodidades</a>
-            </div>
-            <div class="links-item">
-                <a href="portal_adm.php?page=GerenciarUsuários"><img src="../imgs/dev/gerenciamento_users.svg" alt=""></a>
-                <a href="portal_adm.php?page=GerenciarUsuários" class="button">Gerenciamento de usuários</a>
-            </div>
-        </div>
-    </section>
     <?php include $page;?>
     
 </body>
