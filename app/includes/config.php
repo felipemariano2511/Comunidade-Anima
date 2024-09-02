08e8c3e94a9af11fcd90128ce27433b7bddf98d2
Felipe Mariano <felipe.mariano2511@gmail.com>
2024-09-02 08:48:25 -0300

    Update

--- app/includes/config.php
+++ app/includes/config.php
@@ -1,6 +1,6 @@
 <?php
 
-    $con = mysqli_connect("localhost:3500", "root", "");
+    $con = mysqli_connect("localhost:3306", "root", "");
     $database = mysqli_select_db($con,"comunidade_anima");
 
     if (!$con || !$database) echo mysqli_error($con);
--- database/database.php
+++ database/database.php
@@ -1,5 +1,5 @@
 <?php
-$servername = "localhost:3500";
+$servername = "localhost:3306";
 $username = "root";
 $password = "";
 $dbname = "comunidade_anima";
