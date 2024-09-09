0443e4d95089625a14e5388fa75e5c21158cc3f9
Felipe Mariano <136535564+felipemariano2511@users.noreply.github.com>
2024-09-09 19:49:23 -0300

    Update config.php

--- app/includes/config.php
+++ app/includes/config.php
@@ -3,6 +3,9 @@
     $con = mysqli_connect("localhost:3306", "root", "");
     $database = mysqli_select_db($con,"comunidade_anima");
 
+    $con->set_charset("utf8mb4");
+    
     if (!$con || !$database) echo mysqli_error($con);
+        
 
 ?>
